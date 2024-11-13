fbcaef9ebfda10f8a4d5b7755c191541133580d6
Franco Rodriguez <francogabriel639@gmail.com>
2024-11-12 21:54:53 -0300

    Fetch a la api e impresion de la info.
    No pude hacer que se vean las imagenes, pero estamos en trabajo

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+import Movies from "./Movies";
+
+function MovieCard() {
+    return (
+        <div className="row">
+            <Movies />
+        </div>
+    );
+}
+
+export default MovieCard;
---
+++ src/components/Movies.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+function Movies() {
+    const [movies, setMovies] = useState([]);
+    
+    useEffect(() => {
+        console.log("El componente se acaba de montar (primer render).");
+
+        async function getMovies() {
+            const response = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=004d65b10e415d795c9d86a817745e22&include_adult=false&page=1&sort_by=popularity.desc&vote_count.gte=40");
+            const moviesData = await response.json();
+            setMovies(moviesData.results); 
+        }
+
+        getMovies();
+    }, []);
+
+    return (
+        <div className="col-3">
+            {movies.map((movie) => (
+                <div key={movie.id}
+                style={{ width:"150px", height: "300px" }}>
+                    {movie.poster_phat}
+                    {movie.title}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Movies;
