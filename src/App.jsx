ea8f88161c847955038baf8f1be8bcb371e7b89a
Franco-rodriguez-pereira <franco.pereira@anima.edu.uy>
2024-11-14 20:42:10 -0300

    Changes in App.css
    Mas cambios esteticos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import MovieCard from "./components/MovieCard";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="allContainer">
+      <div className="header">
+        <Header />
+      </div>
       <div className="container">
-        <MovieCard />
+        <div>
+          <MovieCard />
+        </div>
       </div>
     </div>
   );
