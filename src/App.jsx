6ecb4ce44a5677f1346ecf3e955fa1e148235a3a
Franco-rodriguez-pereira <franco.pereira@anima.edu.uy>
2024-11-21 21:45:52 -0300

    Rutas
    Sin terminar(parte 5)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import "./App.css";
 import Home from "./web/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Recommend } from "@mui/icons-material";
+import Recommendation from "./components/Recommendation";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
+    },
+    {
+      path: "/Recommendation",
+      element: <Recommendation />,
     },
   ]);
   return <RouterProvider router={router} />;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <header>
       <h1>HACKFLIX</h1>
+
+      <ul>
+        <li>
+          <Link to="/">Recomendaciones</Link>
+        </li>
+      </ul>
     </header>
   );
 }
